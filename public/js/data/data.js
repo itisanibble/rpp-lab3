49142138b80a12783b7cb4d06de333bf4a8e7666
Four Bits <59286938+itisanibble@users.noreply.github.com>
2022-10-30 16:19:18 -0300

    "cantidadPuertas":1

--- public/js/data/data.js
+++ public/js/data/data.js
@@ -1,6 +1,6 @@
 const content = `[{"id":14, "modelo":"Ferrari F100", "añoFabricacion":1998, "velocidadMaxima":400, "cantidadPuertas":2, "cantidadRuedas":4},{"id":51, "modelo":"Dodge Viper", "añoFabricacion":1991, "velocidadMaxima":266, "cantidadPuertas":2, "cantidadRuedas":4},{"id":67, "modelo":"Boeing CH-47 Chinook",
 "añoFabricacion":1962, "velocidadMaxima":302, "alturaMaxima":6, "autonomia":1200},{"id":666, "modelo":"Aprilia RSV 1000 R",
-"añoFabricacion":2004, "velocidadMaxima":280, "cantidadPuertas":0, "cantidadRuedas":2},{"id":872, "modelo":"Boeing 747-400", "añoFabricacion":1989,
+"añoFabricacion":2004, "velocidadMaxima":280, "cantidadPuertas":1, "cantidadRuedas":2},{"id":872, "modelo":"Boeing 747-400", "añoFabricacion":1989,
 "velocidadMaxima":988, "alturaMaxima":13, "autonomia":13450},{"id":742, "modelo":"Cessna CH-1 SkyhookR", "añoFabricacion":1953,
 "velocidadMaxima":174, "alturaMaxima":3, "autonomia":870}]`;
 export default content;
